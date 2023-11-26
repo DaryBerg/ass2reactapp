132cb49b83dd2cabbc059ff9dd08b3323f263eda
micalev <alevi561@mtroyal.ca>
2023-11-26 03:39:25 -0700

    Completed SkillsSection.js

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './SkillsSection.css'; // Import your CSS file
 
 const SkillsSection = () => {
+  const [showSkills, setShowSkills] = useState(false);
+
   useEffect(() => {
     const skillsBars = document.querySelectorAll('.skill-bar');
 
     const options = {
-      threshold: 0.8,
+      threshold: 0.8
     };
 
     const skillObserver = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
+      entries.forEach(entry => {
         if (entry.isIntersecting) {
           entry.target.style.width = entry.target.dataset.progress;
           observer.unobserve(entry.target);
@@ -17,29 +20,48 @@ const SkillsSection = () => {
       });
     }, options);
 
-    skillsBars.forEach((bar) => {
+    skillsBars.forEach(bar => {
       skillObserver.observe(bar);
     });
 
+    setShowSkills(true); // Once component mounts, set to true to show skills
+
     return () => {
-      skillObserver.disconnect();
+      skillsBars.forEach(bar => {
+        skillObserver.unobserve(bar);
+      });
     };
-  }, []);
+  }, []); // Run only once on component mount
 
   return (
     <section className="skills-section">
-      <div className="container">
-        <h2>Skills</h2>
-        <div className="skill">
-          <span className="skill-label">HTML</span>
-          <div className="skill-bar" data-progress="90%"></div>
-        </div>
-        <div className="skill">
-          <span className="skill-label">CSS</span>
-          <div className="skill-bar" data-progress="80%"></div>
-        </div>
-        {/* Add more skills with similar structure */}
-      </div>
+      {showSkills && (
+            <section class="skills-section">
+            <div class="container">
+                <h2>Skills</h2>
+                <div class="skill">
+                    <span class="skill-label">HTML</span>
+                    <div class="skill-bar" data-progress="90%"></div>
+                </div>
+                <div class="skill">
+                    <span class="skill-label">CSS</span>
+                    <div class="skill-bar" data-progress="80%"></div>
+                </div>
+                <div class="skill">
+                    <span class="skill-label">Java</span>
+                    <div class="skill-bar" data-progress="70%"></div>
+                </div>
+                <div class="skill">
+                    <span class="skill-label">React</span>
+                    <div class="skill-bar" data-progress="0%"></div>
+                </div>
+                <div class="skill">
+                    <span class="skill-label">Python</span>
+                    <div class="skill-bar" data-progress="40%"></div>
+                </div>
+            </div>
+        </section>
+      )}
     </section>
   );
 };
